2075d5af7b84f5813ce41193ef20c23760955789
15027122549 <15027122549@189.cn>
2019-08-23 16:15:09 +0800

    c调用java的方法

--- Project4/c调用java的方法.c
+++ Project4/c调用java的方法.c
@@ -2,5 +2,8 @@
 //c调用java的方法
 JNIEXPORT void JNICALL Java_ndk1_Simple4_callAddMethod
 (JNIEnv * env, jobject jobj) {
-
+	jclass jclazz = (*env)->GetObjectClass(env, jobj);
+	jmethodID jmethod_id = (*env)->GetMethodID(env, jclazz, "add", "(II)I");
+ jint sum=(*env)->CallIntMethod(env, jobj,jmethod_id , 23, 1098);
+ printf("sum=%d", sum);
 }
